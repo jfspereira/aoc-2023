bf5264aa3dcc185a7750eacd6cc9620fedfdfd02
Jorge <j.filipe95@gmail.com>
2023-12-01 00:08:19 +0000

    Cleanup

--- src/main/kotlin/d1/Day01.kt
+++ src/main/kotlin/d1/Day01.kt
@@ -1,9 +1,8 @@
 package d1
 
-import InputFileLocator.DayConstantEnum.DAY_1;
+import InputFileLocator.DayConstantEnum.DAY_1
 import InputFileLocator.Companion.getExample1
 import InputFileLocator.Companion.getExample2
-import println
 import readInput
 
 fun main() {
@@ -11,16 +10,16 @@ fun main() {
         val testInput = readInput(getExample1(DAY_1))
 
 
-        testInput.println()
+        println("Part 1 result: $testInput")
     }
 
     fun part2() {
         val testInput = readInput(getExample2(DAY_1))
 
 
-        testInput.println()
+        println("Part 2 result: $testInput")
     }
 
-    println("Part 1 result: ${part1().println()}")
-    println("Part 2 result: ${part2().println()}")
+    part1()
+    part2()
 }
